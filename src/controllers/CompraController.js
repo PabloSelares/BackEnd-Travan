54f19ee9470406e58d85a0cb198308a8b52e4734
Gabryell Leal Rocha <gabryell.rocha@maisunifacisa.com.br>
2025-04-10 15:01:21 -0300

    refactor: refaactoration of the response when all Compras are fetch and when a Compra if create with an invalid ProductId

--- src/controllers/CompraController.js
+++ src/controllers/CompraController.js
@@ -1,19 +1,40 @@
 import Compra from '../models/CompraModel.js';
+import Product from '../models/ProductModel.js';
 
 const compraController = {
     create: async (req, res) => {
         try {
-            const compra = req.body;
-            const newCompra = await Compra.create(compra);
-            res.status(201).json({ message: 'Compra realizada com sucesso', newCompra });
+            const id = req.body.produto;
+            if (!id) throw new Error('ID do produto não fornecido');
+            let produto = await Product.find({ _id: id });
+            if (!produto) throw new Error('Produto não encontrado');
+            let compra = {
+                produto: id
+            };
+            compra = await Compra.create(compra);
+            res.status(201).json({ message: 'Compra realizada com sucesso', compra });
         } catch (error) {
+            if (error.message.includes('Cast to ObjectId failed for value')) {
+                return res.status(400).json({ message: 'ID do produto inválido' });
+            }
             res.status(500).json({ message: error.message });
         }
     },
     getAll: async (req, res) => {
         try {
-            const compras = await Compra.find().populate("produto");
-            res.status(200).json(compras);
+            const compras = await Compra.find().populate("produto").lean();
+    
+            const comprasFiltradas = compras.map(compra => {
+                const { __v, ...compraSemV } = compra;
+                const { createdAt, updatedAt, __v: __vProduto, ...produtoFiltrado } = compra.produto || {};
+    
+                return {
+                    ...compraSemV,
+                    produto: produtoFiltrado
+                };
+            });
+    
+            res.status(200).json(comprasFiltradas);
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
