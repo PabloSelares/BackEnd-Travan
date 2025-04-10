cf2b06fb111a156f30b60a9c4c6922ecd59dcef8
Gabryell Leal Rocha <gabryell.rocha@maisunifacisa.com.br>
2025-04-10 14:22:14 -0300

    refactor: type of desconto attribute

--- src/models/ProductModel.js
+++ src/models/ProductModel.js
@@ -4,7 +4,7 @@ const Schema = new mongoose.Schema({
     origem: { type: String, required: true },
     destino: { type: String, required: true },
     preco: { type: Number, required: true },
-    desconto: { type: String },
+    desconto: { type: Number },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
 });
