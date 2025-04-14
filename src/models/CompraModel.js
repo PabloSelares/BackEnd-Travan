7bc9f86f1eef70fb2f8214b1a5c602a8f4d33943
Gabryell Leal Rocha <gabryell.rocha@maisunifacisa.com.br>
2025-04-14 19:17:53 -0300

    feat: das novas caracteristicas de uma compra

--- src/models/CompraModel.js
+++ src/models/CompraModel.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 
 const Schema = new mongoose.Schema({
     produto: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+    comprador: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    message: { type: String },
     createdAt: { type: Date, default: Date.now }
 })
 
